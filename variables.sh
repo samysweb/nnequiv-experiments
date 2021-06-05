40005dd11a382886f448f21b2c5e1548e0ed43f1
Samuel Teuber <samuel@samweb.org>
2021-06-05 20:09:47 +0200

    Preparing rerun of experiment due to timeout (now smaller property)

--- variables.sh
+++ variables.sh
@@ -10,7 +10,7 @@ INSTANCES_BUENING=instances-buening.csv
 RUN_COUNT=1
 
 # NNEquiv Commit
-NNEQUIV_COMMIT="e96d93b"
+NNEQUIV_COMMIT="48e7b4a"
 BUENING_COMMIT="a5c91be"
 
 TO=32400 # 9h per instance due to intense computations...
