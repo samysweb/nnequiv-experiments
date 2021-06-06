6681eee1bb7216b22232d6e20de66851f74d757f
Samuel Teuber <samuel@samweb.org>
2021-06-06 16:14:20 +0200

    Retrying with now hopefully fully working version

--- variables.sh
+++ variables.sh
@@ -10,7 +10,7 @@ INSTANCES_BUENING=instances-buening.csv
 RUN_COUNT=1
 
 # NNEquiv Commit
-NNEQUIV_COMMIT="dd0216a"
+NNEQUIV_COMMIT="937e10f"
 BUENING_COMMIT="a5c91be"
 
 TO=32400 # 9h per instance due to intense computations...
