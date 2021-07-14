1937276eaba3dee70a39ffb000256ce1a4a9a174
Samuel Teuber <samuel@samweb.org>
2021-07-14 17:41:59 +0200

    Preparing counterexample search experiment

--- variables.sh
+++ variables.sh
@@ -8,17 +8,17 @@ if [[ "$(hostname)" = "hal9000" ]]; then
 	TMPDIR=/tmp/steuber/experiment
 fi
 if [[ "$(hostname)" = "baldur3" ]]; then
-	TMPDIR=/raid/steuber/tmp2
+	TMPDIR=/raid/steuber/tmp3
 fi
-INSTANCES=instances-tightness.csv
-INSTANCES_BUENING=instances-tightness-buening.csv
+INSTANCES=instances-counterexamples.csv
+INSTANCES_BUENING=instances-counterexamples-buening.csv
 
 # How often are benchmarks run?
 RUN_COUNT=3
 
 # NNEquiv Commit
-NNEQUIV_COMMIT="3ad4254"
+NNEQUIV_COMMIT="06a2d52"
 BUENING_COMMIT="04f3bfb" # speedup branch (modified for ONNX Parsing)
 
-TO=10800
+TO=120
 MO=512
